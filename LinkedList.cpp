a3e54740f4a4d9b46efcfd817d00dd78575114ab
Dhananjay Ingole <145216339+dhananjayingole@users.noreply.github.com>
2026-09-09 15:30:39 +0530

    Refactor LinkedList operations and add new features
    Refactor LinkedList operations for improved readability and efficiency. Added unordered_set for intersection and implemented addition of two linked lists.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include<stack>
-#include<vector>
-#include<bits/stdc++.h>
+#include <stack>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
@@ -28,8 +29,7 @@ void printList(ListNode* head) {
 ListNode* InsertAtHead(ListNode* head, int val) {
     ListNode* newNode = new ListNode(val);
     newNode->next = head;
-    head = newNode;
-    return head;
+    return newNode;
 }
 
 ListNode* InsertAtTail(ListNode* head, int val) {
@@ -47,7 +47,8 @@ ListNode* InsertAtTail(ListNode* head, int val) {
     return head;
 }
 
-ListNode* DeleteAthead(ListNode* head){
+ListNode* DeleteAthead(ListNode* head) {
+    if (head == nullptr) return nullptr;
     ListNode* curr = head;
     head = head->next;
     delete curr;
@@ -55,68 +56,67 @@ ListNode* DeleteAthead(ListNode* head){
     return head;
 }
 
-ListNode* DeleteAttail(ListNode* head){
-    ListNode* curr = head;
+ListNode* DeleteAttail(ListNode* head) {
+    if (head == nullptr) return nullptr;
+    if (head->next == nullptr) {
+        delete head;
+        return nullptr;
+    }
 
-    while(curr->next->next != nullptr){
+    ListNode* curr = head;
+    while (curr->next->next != nullptr) {
         curr = curr->next;
     }
 
+    delete curr->next;
     curr->next = nullptr;
 
     return head;
 }
 
-int SearchNodeIdx(ListNode* head, int key){
+int SearchNodeIdx(ListNode* head, int key) {
     ListNode* curr = head;
     int cnt = 0;
 
-    while(curr != nullptr){
-        curr = curr->next;
-        cnt++;
-
-        if(curr->data == key){
+    while (curr != nullptr) {
+        if (curr->data == key) {
             return cnt;
         }
+        curr = curr->next;
+        cnt++;
     }
     return -1;
 }
 
 ListNode* DeleteByVal(ListNode* head, int key) {
-    // 1. If list is empty
-    if (head == nullptr) {
-        return nullptr;
-    }
+    if (head == nullptr) return nullptr;
 
-    // 2. If the head node itself holds the key
     if (head->data == key) {
         ListNode* temp = head;
-        head = head->next; // Move head to the next node
-        delete temp;       // Free memory
+        head = head->next;
+        delete temp;
         return head;
     }
 
-    // 3. Search for the key to delete, keeping track of previous node
     ListNode* curr = head;
     while (curr->next != nullptr && curr->next->data != key) {
         curr = curr->next;
     }
 
-    // 4. Key was found (curr->next is the node to delete)
     if (curr->next != nullptr) {
         ListNode* temp = curr->next;
-        curr->next = curr->next->next; // Unlink the node from list
-        delete temp;                   // Free memory
+        curr->next = curr->next->next;
+        delete temp;
     }
 
     return head;
 }
 
-int lengthofLinkedlist(ListNode* head){
+int lengthofLinkedlist(ListNode* head) {
     ListNode* curr = head;
-    int len=0;
+    int len = 0;
 
-    while(curr != nullptr){
+    while (curr != nullptr) {
         curr = curr->next;
         len++;
     }
@@ -137,7 +137,7 @@ ListNode* middleofLL(ListNode* head) {
 }
 
 ListNode* reverseLL(ListNode* head) {
-    stack<int> st; // Store integer values instead of pointers
+    stack<int> st;
 
     ListNode* curr = head;
     while (curr != nullptr) {
@@ -154,66 +154,60 @@ ListNode* reverseLL(ListNode* head) {
     return head;
 }
 
-bool DetectLoop(ListNode* head){
+bool DetectLoop(ListNode* head) {
     ListNode* slow = head;
     ListNode* fast = head;
 
-    while(fast != nullptr && fast->next != nullptr){
+    while (fast != nullptr && fast->next != nullptr) {
         slow = slow->next;
         fast = fast->next->next;
 
-        if(slow == fast){
+        if (slow == fast) {
             return true;
         }
     }
     return false;
 }
 
-ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        bool isCycle = false;
+ListNode* detectCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    bool isCycle = false;
 
-        while(fast != NULL && fast->next !=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast){
-                isCycle = true;
-                break;
-            }
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            isCycle = true;
+            break;
         }
-        if(!isCycle){
-            return NULL;
-        }
+    }
+    if (!isCycle) {
+        return NULL;
+    }
 
-        //  to find the starting point of the cycle 
-        // hum slow ko fir se reinitilise karenge from head 
-        // and iss baar slow and fast dono ko ek se hi update karte jayenge.
-         slow = head;
-        while(slow != fast){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        //jaha par slow and fast mil gaye matlab slow == fast ho gaya usko return karenge as slow.
-        // jab tak nahi milta tab tak slow and fast ko +1 se update karenge.
-        return slow;
+    slow = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow;
 }
 
-bool isPalindrome(ListNode* head){
-    vector<int>vec;
-
+bool isPalindrome(ListNode* head) {
+    vector<int> vec;
     ListNode* curr = head;
 
-    while(curr != nullptr){
+    while (curr != nullptr) {
         vec.push_back(curr->data);
         curr = curr->next;
     }
 
-    int left =0;
-    int high = vec.size()-1;
+    int left = 0;
+    int high = vec.size() - 1;
 
-    while(left < high){
-        if(vec[left] != vec[high]){
+    while (left < high) {
+        if (vec[left] != vec[high]) {
             return false;
         }
         left++;
@@ -222,69 +216,118 @@ bool isPalindrome(ListNode* head){
     return true;
 }
 
-ListNode* SortLL(ListNode* head){
-        vector<int>vec;
-        ListNode* curr = head;
-        while(curr != nullptr){
-            vec.push_back(curr->data);
-            curr = curr->next;
-        }
+ListNode* SortLL(ListNode* head) {
+    vector<int> vec;
+    ListNode* curr = head;
+    while (curr != nullptr) {
+        vec.push_back(curr->data);
+        curr = curr->next;
+    }
 
-        sort(vec.begin(), vec.end());
+    sort(vec.begin(), vec.end());
 
-        curr = head;
-        int idx = 0;
-        while(curr != nullptr){
-            curr->data = vec[idx++];
-            curr = curr->next;
-        }
-        return head;
+    curr = head;
+    int idx = 0;
+    while (curr != nullptr) {
+        curr->data = vec[idx++];
+        curr = curr->next;
+    }
+    return head;
 }
 
+ListNode* getIntersection(ListNode* headA, ListNode* headB) {
+    unordered_set<ListNode*> st;
+
+    ListNode* currA = headA;
+    while (currA != nullptr) {
+        st.insert(currA);
+        currA = currA->next;
+    }
+
+    ListNode* currB = headB;
+    while (currB != nullptr) {
+        if (st.find(currB) != st.end()) {
+            return currB;
+        }
+        currB = currB->next;
+    }
+    return nullptr;
+}
+
+ListNode* AddtwoLinkedList(ListNode* head1, ListNode* head2) {
+    ListNode* DummyNode = new ListNode(-1);
+    ListNode* curr = DummyNode;
+    int carry = 0;
+
+    ListNode* l1 = head1;
+    ListNode* l2 = head2;
+
+    while (l1 != nullptr || l2 != nullptr || carry != 0) {
+        int sum = carry;
+        if (l1 != nullptr) {
+            sum += l1->data;
+            l1 = l1->next;
+        }
+        if (l2 != nullptr) {
+            sum += l2->data;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        curr->next = new ListNode(sum % 10);
+        curr = curr->next;
+    }
+    return DummyNode->next;
+}
 
 int main() {
-    // Correct node initialization using heap allocation
     ListNode* head = new ListNode(1);
     head->next = new ListNode(2);
     head->next->next = new ListNode(3);
 
-    // Get and print middle value
+    ListNode* head2 = new ListNode(5);
+    head2->next = new ListNode(9);
+    head2->next->next = new ListNode(4);
+
+    cout << "Sum of Two LinkedList is: ";
+    ListNode* Sum = AddtwoLinkedList(head, head2);
+    printList(Sum);
+
     ListNode* mid = middleofLL(head);
     if (mid != nullptr) {
-        cout << "Middle Value: " << mid->data << endl; // Output: 2
+        cout << "Middle Value: " << mid->data << endl;
     }
     
-    // Demonstration of helper functions
-    head = InsertAtHead(head, 0);   // List: 0 1 2 3
-    head = InsertAtTail(head, 4);   // List: 0 1 2 3 4
+    head = InsertAtHead(head, 0);   
+    head = InsertAtTail(head, 4);   
 
-    // Correct function call syntax
+    cout << "List after insertions: ";
     printList(head);
 
     int ans = SearchNodeIdx(head, 2);
-    cout<<"Element Key FOund at Index:"<<ans<<endl;
+    cout << "Element Key Found at Index: " << ans << endl;
 
     head = DeleteAthead(head);
     head = DeleteAttail(head);
 
+    cout << "List after head & tail deletion: ";
     printList(head);
 
     head = DeleteByVal(head, 2);
+    cout << "List after deleting value 2: ";
     printList(head);
 
     int length = lengthofLinkedlist(head);
-    cout<<length<<endl;
+    cout << "Length: " << length << endl;
 
-    cout<<"Reversed Linked List is:";
+    cout << "Reversed Linked List is: ";
     head = reverseLL(head);
     printList(head);
 
-    cout<<detectCycle(head)<<endl;
+    cout << "Detect cycle: " << (DetectLoop(head) ? "Yes" : "No") << endl;
 
-    cout<<"is Palindrome aahe ka:";
-    cout<<isPalindrome(head)<<endl;
+    cout << "Is Palindrome: " << (isPalindrome(head) ? "Yes" : "No") << endl;
 
-    cout<<"SOrted LinkedList:";
+    cout << "Sorted LinkedList: ";
     head = SortLL(head);
     printList(head);
 
